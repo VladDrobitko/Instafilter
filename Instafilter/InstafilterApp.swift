6116cffd9d4cad0cda6b1fcfd085e24b94c3c59f
VladDrobitko <boss.drobitko@gmail.com>
2024-12-10 13:58:24 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Владислав Дробитько on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
